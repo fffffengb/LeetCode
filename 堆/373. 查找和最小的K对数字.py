8810e6b47e629a3f2915b982dc4e7784232410aa
fffengb <644683848@qq.com>
2020-04-07 21:18:36 +0800

    修改373为更优解

--- 堆/373. 查找和最小的K对数字.py
+++ 堆/373. 查找和最小的K对数字.py
@@ -1,23 +1,32 @@
 from typing import List
 
 import heapq
+
+
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if len(nums1) == 0 or len(nums2) == 0:
+            return None
         min_heap = []
-        for num1 in nums1:
-            for num2 in nums2:
-                heapq.heappush(min_heap, (num1 + num2, [num1, num2]))
-
         res = []
-        while k != 0 and len(min_heap) != 0:
-            res.append(heapq.heappop(min_heap)[1])
-            k -= 1
-
+        # 先将所有包含nums2[0]的对全部放入堆中, 因为nums1是有序的, 所以它本来就是最小堆
+        for num in nums1:
+            min_heap.append((num + nums2[0], num, nums2[0], 0))
+        while len(min_heap) != 0 and len(res) != k:
+            val, num1, num2, index = heapq.heappop(min_heap)
+            res.append([num1, num2])
+            # 假设当前的(num1, num2)下标为(x, y), 则下一个候选项只会是(x + 1, y)或者(x, y + 1)
+            # 由于在最开始已经将所有nums1数据放入堆, 所以只需要判断(x, y + 1)即可
+            index += 1
+            if index <= len(nums2) - 1:
+                num2 = nums2[index]
+                candidate = (num1 + num2, num1, num2, index)
+                heapq.heappush(min_heap, candidate)
         return res
 
 
-nums1 = [1,7,11]
-nums2 = [2,4,6]
-k = 30
+nums1 = [1, 1, 2]
+nums2 = [1, 2, 3]
+k = 10
 solution = Solution()
 print(solution.kSmallestPairs(nums1, nums2, k))
